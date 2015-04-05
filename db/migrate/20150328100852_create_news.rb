c24559c40a6ed3aa16f0650dffdb251504bd0f7d
Navpreet Singh <navpreet.singh.k@gmail.com>
2015-04-05 23:41:22 +0530

    increasing database string limit

--- db/migrate/20150328100852_create_news.rb
+++ db/migrate/20150328100852_create_news.rb
@@ -1,10 +1,10 @@
 class CreateNews < ActiveRecord::Migration
   def change
     create_table :news do |t|
-      t.string :headline
-      t.string :url
+      t.string :headline, :limit => 1000
+      t.string :url, :limit => 1000
       t.integer :ready
-      t.string :description
+      t.string :description, :limit => 1000
 
       t.timestamps
     end
